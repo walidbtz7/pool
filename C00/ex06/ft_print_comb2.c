8ec0fc90586edd1b9837cf208cfea7f5bdd042f5
Walid Boutzougarte <wboutzou@e2r4p11.1337.ma>
2021-08-09 13:32:10 +0100

    c00

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,63 +1,31 @@
 #include<unistd.h>
-void putchar(char c)
+void	putchar(char c)
 {
 	write(1, &c, 1);
 }
-void ft_print_c(char a, char b, char c, char d){
-	while	(d <= '9')
-	{
-		putchar(a);
-		putchar(b);
-		write(1, " ", 1);
-		putchar(c);
-		putchar(d);
-	     if( a != '9' && b != '8' && c != '9' && d != '9') 
-		 write(1, ", ", 2);
 
-			   
-		d++;
-	}
-}
-
-void	ft_comb(char a, char b, char c ,char d)
-{
-	while	(a <= '9')
-	{
-		while(b <= '8')
-		{
-	 		while(c <= '9')
-	 		{
-			    ft_print_c(a, b, c, d);
-				d = '2';
-				c++;
-				if( c > '9')
-				{
-					b++;
-				}
-			}
-		c = '0';
-		if( b > '8')
-		{
-			a++;
-		}
-	}
-	b = '0';
-  }
-}
 void	ft_print_comb2(void)
 {
-	char	a;
-	char	b;
-	char	c;
-	char	d;
+	int	x;
+	int	y;
 
-	a = '0';
-	b = '0';
-	c = '0';
-	d = '1';
-	ft_comb(a,  b,  c , d);
-}
-int main()
-{
-	ft_print_comb2();
+	x = 0;
+	while (x <= 98)
+	{
+		y = x + 1;
+		while (y <= 99)
+		{
+			putchar('0' + x / 10);
+			putchar('0' + x % 10);
+			putchar(' ');
+			putchar('0' + y / 10);
+			putchar('0' + y % 10);
+			if (x != 98 || y != 99)
+			{
+				write(1, ", ", 2);
+			}
+			y++;
+		}
+		x++;
+	}
 }
---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+#include<stdio.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	int	a;
+
+	if (nb < 0)
+	{
+		 ft_putchar('-');
+		 nb = nb * -1;
+	}
+	a = nb;
+	if (a >= 10)
+	{
+		ft_putnbr(a / 10);
+		ft_putnbr(a % 10);
+	}
+	else
+		ft_putchar(a + '0');
+}
